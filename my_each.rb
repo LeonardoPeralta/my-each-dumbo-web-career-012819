63db3329f9c2ac15b3073904926ae66ae012b5a9
Leonardo Peralta <leo.pm15@gmail.com>
2019-01-19 06:33:46 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,5 +1,15 @@
 def my_each(collection)
-  collection.each { |item| puts item}
-end
+#  collection.each { |item| puts item}
+# names.each { |name| puts "#{name}"}
+if block_given?
+i = 0
 
-my_each(collection)
+    while i < names.length
+      yield(names[i])
+      i = i + 1
+    end
+    names
+  else
+    puts "Hey! No block was given!"
+  end
+end
